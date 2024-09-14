a3eb8329039712424812a6cc3ad5aa81b3ea2b1a
Xounder <renanfellipe1@hotmail.com>
2024-09-13 22:11:50 -0300

    created game logic

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../../public/assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  title:string = ''
+  result:string = ''
+  questions: any = ''
+  questionSelected:any = ''
 
+  questionIndex:number = 0
+
+  finished:boolean = false
+
+  answers:string[] = []
+
+  ngOnInit():void {
+    if (quizz_questions) {
+      this.title = quizz_questions.title
+      this.questions = quizz_questions.questions
+      this.changeQuestion()
+    }
+  }
+
+  changeQuestion():void {
+    this.questionSelected = this.questions[this.questionIndex]
+    this.questionIndex++
+  }
+
+  playerChoose(value:string):void{
+    this.answers.push(value)
+    this.changeQuestion()
+    this.isEndGame()
+  }
+
+  isEndGame():void {
+    if (this.questionIndex === this.questions.length){
+      this.finished = true
+      if (this.answers.filter(x => x == "A").length > this.answers.filter(x => x == 'B').length){
+        this.result = quizz_questions.results.A
+      } else {
+        this.result = quizz_questions.results.B
+      }
+    }
+  }
 }
